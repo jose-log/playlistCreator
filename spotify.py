b3f3939dd7bce5828da2ada8e6a56ced247d6f6e
Jose Logreira <jose.logreira@gmail.com>
2020-05-05 19:47:08 -0500

    All spotify functions are working using the OAuth token generated in the Web API console.

--- spotify.py
+++ spotify.py
@@ -14,28 +14,75 @@ from spotify_secrets import client_app_secret as app_secret
 serviceurl = 'https://api.spotify.com/v1'
 authurl = 'https://accounts.spotify.com/authorize'
 
-def create_playlist():
-    
-    request_body = json.dumps({
-        "name": "Youtube Liked Vids",
-        "description": "All Liked Youtube Videos",
-        "public": True
-    })
+###############################################################################
+# 	POST REQUESTS
+###############################################################################
 
-    query = "https://api.spotify.com/v1/users/{}/playlists".format(
-        spotify_user_id)
-    response = requests.post(
-        query,
-        data=request_body,
-        headers={
-            "Content-Type": "application/json",
-            "Authorization": "Bearer {}".format(spotify_token)
-        }
-    )
-    response_json = response.json()
+def create_new_playlist(pl_name):
 
-    # playlist id
-    return response_json
+	endpoint = 'https://api.spotify.com/v1/users/{}/playlists'.format(usr_id)
+
+	parameters = {
+		"name": pl_name,
+		"description": "Liked Youtube Videos",
+		"public": True
+	}
+	head = {
+		'Accept':'application/json',
+		"Content-Type": "application/json",
+		"Authorization": "Bearer {}".format(oauth_token)
+	}
+	payload = json.dumps(parameters)
+
+	print('Creating new playlist...')
+	response = requests.post(endpoint, data=payload, headers=head)
+	response_json = response.json()
+
+	try:
+		# playlist id
+		out = response_json['id']
+		print('>>New playlist \"' + pl_name + '\" created:')
+	except:
+		print(json.dumps(response_json, sort_keys=True, indent=4))
+		print('ERROR:', response_json['error']['status'])
+		print('message:', response_json['error']['message'])
+		out = response_json['error']['status']
+
+	return out
+
+def insert_into_playlist(pl_id, song_uri):
+
+	endpoint = 'https://api.spotify.com/v1/playlists/{}/tracks'.format(pl_id)
+	
+	parameters = {
+		'uris':['{}'.format(song_uri)]
+	}
+	head = {
+		'Accept':'application/json',
+		'Content-Type': 'application/json',
+		'Authorization': 'Bearer {}'.format(oauth_token)
+	}
+	payload = json.dumps(parameters)
+
+	print('Adding item to playlist...')
+	response = requests.post(endpoint, data=payload, headers=head)
+	response_json = response.json()
+
+	try:
+		# playlist id
+		out = response_json['snapshot_id']
+		print('>>Track \"' + song_uri + '\" added to \"' + pl_id + '\" playlist')
+	except:
+		print(json.dumps(response_json, sort_keys=True, indent=4))
+		print('ERROR:', response_json['error']['status'])
+		print('message:', response_json['error']['message'])
+		out = response_json['error']['status']
+
+	return 0
+
+###############################################################################
+# 	GET REQUESTS
+###############################################################################
 
 def retrieve_user_playlists():
 
@@ -50,20 +97,26 @@ def retrieve_user_playlists():
 		'Content-Type':'application/json',
 		'Authorization':'Bearer {}'.format(oauth_token)
 	}
-	response = requests.get(endpoint, params=parameters, headers=head)
 
+	print('Retrieving user playlists...')
+	response = requests.get(endpoint, params=parameters, headers=head)
 	response_json = response.json()
     
-	# Dump JSON response to screen
-	# print(json.dumps(response_json, sort_keys=True, indent=4))
-	print('Playlists Count: ', response_json['total'])
+	try:
+		print('>> Playlists Count: ', response_json['total'])
+	except:
+		# Dump JSON response to screen
+		print(json.dumps(response_json, sort_keys=True, indent=4))
+		print('ERROR:', response_json['error']['status'])
+		print('message:', response_json['error']['message'])
+		exit()
 
 	c = 0
-	pl = []
+	pl = {}
 	for i in response_json['items']:
 	    # Store data into the data base
 	    print(' > Playlist {}: {} | Songs = {} | ID: {}'.format(c + 1, i['name'], i['tracks']['total'], i['id']))
-	    pl.append(i['name'])
+	    pl[i['name']] = i['id']
 	    c += 1
 
 	# Back up json response
@@ -72,21 +125,63 @@ def retrieve_user_playlists():
 	fh.close()
 	# Back up the filtered playlist names
 	fh = open('spotify_pl.txt', 'w')
-	for i in pl:
-		fh.write(i + '\n')
+	for i,j in pl.items():
+		fh.write(i + ',' + j + '\n')
 	fh.close()
 	
 	return pl
 
+def search_catalog(track, artist):
+
+	endpoint = 'https://api.spotify.com/v1/search'
+
+	parameters = {
+		'q':'track:{} artist:{}'.format(track, artist),
+		'type':'track',
+		'limit':'3'
+	}
+	head = {
+		'Accept':'application/json',
+		'Content-Type':'application/json',
+		'Authorization':'Bearer {}'.format(oauth_token)
+	}
+
+	print('Searching in spotify catalog...')
+	response = requests.get(endpoint, params=parameters, headers=head)
+	response_json = response.json()
+
+	try:
+		songs = response_json["tracks"]["items"]
+		# only use the first song
+		out = songs[0]["uri"]
+		print('Search FINISHED!')
+	except:
+		print(json.dumps(response_json, sort_keys=True, indent=4))
+		print('ERROR:', response_json['error']['status'])
+		print('message:', response_json['error']['message'])
+		out = response_json['error']['status']
+		
+	return out
+
+###############################################################################
+# 	MISC
+###############################################################################
+
 def read_user_playlists():
 
-	pl = []
+	pl = {}
 	fh = open('spotify_pl.txt')
 	for line in fh:
-		pl.append(line)
+		line = line.rstrip('\n')
+		x = line.split(',')
+		pl[x[0]] = x[1]
 	fh.close()
 
 	return pl
+
+###############################################################################
+# 	MAIN PROGRAM
+###############################################################################
 
 if __name__ == '__main__':
     
@@ -117,7 +212,12 @@ if __name__ == '__main__':
 
 	new_playlist = 'Youtube Liked Vids'
 
-	if new_playlist not in pl:
+	if new_playlist not in pl.keys():
 		print('Creating new Youtube playlist')
+		pl_id = create_new_playlist(new_playlist)
 	else:
 		print('Youtube playlist already exists')
+		pl_id = pl[new_playlist]
+
+	track_uri = (search_catalog('Oceans','Hillsong'))
+	insert_into_playlist(pl_id, track_uri)
